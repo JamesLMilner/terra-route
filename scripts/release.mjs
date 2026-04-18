55da45c6378d4a1a5dfe694c773d36ac18b0eee5
James Milner <jamesmilner22@hotmail.com>
2026-04-18 15:29:17 +0100

    chore: fix release script

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -16,6 +16,15 @@ export default (packageName, packageJsonPath, changelogPath) => ({
             return commit;
         },
     },
+    scripts: {
+        // After we bump but before we commit, we want to update all the other packages that have a dependency on the package we just bumped
+        // to make sure they are using the correct version. We also want to update the package-lock.json file to make sure it is in sync with
+        // the updated package.json files.
+        postbump:
+            "npm install --package-lock-only --no-audit --no-fund && git add package-lock.json",
+    },
+    // Include files staged by postbump in the release commit.
+    commitAll: true,
     changelogFile: changelogPath,
     releaseCommitMessageFormat: `chore: release version {{currentTag}}`,
 });
